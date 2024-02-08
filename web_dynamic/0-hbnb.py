be4cd03a0fe50f777ad5b50de73af3684fd0503c
Coderyounes <younesanas2021@gmail.com>
2024-02-08 20:57:15 +0100

    add the cache_id variable & update the template

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+from uuid import uuid4
 from models import storage
 from models.state import State
 from models.city import City
@@ -18,13 +19,14 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
-
+    cache_id = uuid4()
+    print(cache_id)
     for state in states:
         st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
 
@@ -34,10 +36,12 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=cache_id
+                           )
 
 
 if __name__ == "__main__":
