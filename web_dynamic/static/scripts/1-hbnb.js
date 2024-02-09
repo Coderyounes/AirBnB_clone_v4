2192fa4d82a61eee7ca711b3ee59e186ab13aaf3
Coderyounes <younesanas2021@gmail.com>
2024-02-09 01:44:26 +0100

    add the js function to Check the checked box & handle the data

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,10 @@
-test
+$(document).ready(function() {
+    $('input:checkbox').change(function() {
+        let amenities_id = [];
+        $('input:checkbox:checked').each(function() {
+            amenities_id.push($(this).data("name"));
+        });
+        $('div.amenities h4').text(amenities_id.join(', '));
+    });
+});
+
